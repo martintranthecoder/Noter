e013eb02999c064a9d51f4ebb1db1e6307bbb5df
Martin Tran <martintran3003.work@gmail.com>
2023-11-30 14:06:58 -0800

    update main_page

--- app/routes.py
+++ app/routes.py
@@ -85,6 +85,7 @@ def signup():
 @login_required
 def logout():
     logout_user()
+    flash("You have successfully logged out.")
     return redirect(url_for('login'))
 
 @myapp.route('/deleteaccount')
