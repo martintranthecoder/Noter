074c3dedcb60b9ccdcf9b22f2352238d34781424
tqkhai260595 <76671514+tqkhai260595@users.noreply.github.com>
2023-11-30 19:58:13 -0800

    Update __int__.py

--- static/__int__.py
+++ static/__int__.py
@@ -1,17 +1,25 @@
-# __init__.py inside the 'app' directory
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from app.routes import main as main_routes  # Relative import within the 'app' package
+from flask_login import LoginManager
+from flask_ckeditor import CKEditor
 
-db = SQLAlchemy()
+myapp = Flask(__name__)
+myapp.config.from_object(Config)
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(myapp)
 
-    db.init_app(app)
+ckeditor = CKEditor(myapp)
 
-    app.register_blueprint(main_routes)
+login = LoginManager(myapp)
+login.login_view = 'login'
+login.login_message = "Please login to access this page."
+login.refresh_view = 'login'
+login.needs_refresh_message = "Please re-login to access this page."
 
-    return app
+
+with myapp.app_context():
+    from app.models import *
+    db.create_all()
+
+from app import routes
