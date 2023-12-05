2a04418ffa97ca8bac04387f6f0d64e9dc1a4216
tqkhai260595 <76671514+tqkhai260595@users.noreply.github.com>
2023-12-04 19:10:21 -0800

    Update models.py for Attach Images inside the Note Functionality

--- static/models.py
+++ static/models.py
@@ -3,7 +3,11 @@ from app.models import User
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from flask_ckeditor import CKEditorField
-
+# Attach Images inside the Note Functionality
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
+db = SQLAlchemy()
+######
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -17,6 +21,21 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
     submit = SubmitField('Sign Up')
+######################################
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    notes = db.relationship('Note', backref='author', lazy=True)
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    image_url = db.Column(db.String(300))  # New field for storing image URL
+############################################################
     
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -27,7 +46,12 @@ class SignupForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError("Email already exists! Please use a different email address.")
-        
+#################################################################
+def __repr__(self):
+        return f'<User {self.username}>'
+def __repr__(self):
+        return f"Note('{self.title}', '{self.date_posted}')"    
+###############################################################################
 #Add Note Form
 class AddNoteForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
