8d13b40888083582fbe1df5a4587214e70698628
hannahta420 <144192598+hannahta420@users.noreply.github.com>
2023-12-04 19:51:26 -0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,37 @@
+from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    username = db.Column(db.String(64), index=True, unique = True)
+    email = db.Column(db.String(120), index=True, unique = True)
+    password_hash = db.Column(db.String(128))
+    notes = db.relationship('Note', backref='author', lazy='dynamic') 
+    #autosave_time = db.Column(db.Integer, default=30)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    
+    def __repr__(self):
+        return f'<User {self.id}: {self.username}>'
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return f'<Note {self.id}: {self.title}>'
