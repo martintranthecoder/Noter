5b2235000343672277184ce175e89b827a773519
hannahta420 <144192598+hannahta420@users.noreply.github.com>
2023-12-06 16:02:16 -0800

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm
+from app.models import User, Note
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from flask_ckeditor import CKEditorField
+import time
+
+#Login form
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+#Sign Up/Create new account
+class SignupForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email("Please enter a valid email address")])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
+    submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already exists! Please use a different username.")
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email already exists! Please use a different email address.")
+        
+#Add Note Form
+class AddNoteForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField('Create')
+
+#Note Saving for Editing    
+class SaveNoteForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    body = CKEditorField('Body', validators=[DataRequired()])
+    time = time.time()
+    submit = SubmitField('Save')
+
+#Password reset if forgot password
+class ForgotPassword(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()]) 
+    new_password = PasswordField('New Password', validators=[DataRequired()]) 
+    confirm_new = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password', message="Passwords must match")])
+    submit = SubmitField('Submit')
